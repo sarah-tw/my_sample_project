add4c5ccde7700ba22df12b1e9c89e53e08c30c1
Si Xiaojing <xjsi@thoughtworks.com>
2012-05-27 15:21:18 +0800

    add some test for sign up

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -12,6 +12,13 @@ describe "User Pages" do
       it "should not create a user" do
          expect { click_button submit }.not_to change(User, :count)
        end
+     
+        describe "after submission" do
+            before{click_button submit}
+            
+            it {should have_selector('title',text:'Sign up')}
+            it {should have_content('error')}
+           end
     end
     
     describe "with valid information" do
@@ -25,6 +32,13 @@ describe "User Pages" do
       it "should create a user" do
          expect { click_button submit }.to change(User, :count).by(1)
        end
+       
+       describe "after saving the user" do
+          before {click_button submit}
+          let(:user){User.find_by_email('sixiaojing@163.com')}
+          it { should have_selector('div.alert.alert-success', text:'Welcome')}
+          it { should have_selector('title', text:user.name)} 
+       end 
     end
     
     it { should have_selector('h1', text:'Sign up')}
